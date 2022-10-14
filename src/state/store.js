c9722c5d9c1a984a96401f3d3d531cda575fe6bd
mohitrawat111 <109884777+mohitrawat111@users.noreply.github.com>
2022-10-14 20:46:03 +0530

    76-Creating a Redux Store

---
+++ src/state/store.js
@@ -0,0 +1,7 @@
+import { applyMiddleware, configureStore } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+import reducers from "./reducers"
+
+
+
+export const store = configureStore(reducers, {}, applyMiddleware(thunk))
